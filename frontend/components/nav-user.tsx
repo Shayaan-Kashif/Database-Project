7064d7b00f33f2cbf73e75ed8c419d8b0869bd0a
Shayaan Kashif <shayaankashif@mac.home>
2025-11-14 23:58:22 -0500

    Added user name and role on drop down menu

--- frontend/components/nav-user.tsx
+++ frontend/components/nav-user.tsx
@@ -84,9 +84,9 @@ export function NavUser({
                   <AvatarFallback className="rounded-lg">CN</AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">{user.name}</span>
+                  <span className="truncate font-medium">{name}</span>
                   <span className="text-muted-foreground truncate text-xs">
-                    {user.email}
+                    {role}
                   </span>
                 </div>
               </div>
